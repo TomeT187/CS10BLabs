471d611e92a7e325232a28956469456c2366581d
TomeT187 <tomet187@gmail.com>
2024-01-19 17:48:08 -0800

    Selection sort works

--- PROGRAM 1_ Calculating Coefficient Of Lift/main.cpp
+++ PROGRAM 1_ Calculating Coefficient Of Lift/main.cpp
@@ -16,7 +16,16 @@ void readData(const string &filename, vector<double> &angles, vector<double> &co
 
     fin.close();
 }
-double interpolation(double, const vector<double> &, const vector<double> &);
+double interpolation(double angle, const vector<double> &angles, const vector<double> &coefficients){
+    //return f(b) = f(a) + ((b - a)/(c - a))(f(c) - f(a))
+
+    //if 
+    for (unsigned int i = 1; i < angles.size(); i++){
+        if (angle == angles[i]){
+            return coefficients[i];
+        }
+    }
+}
 
 bool isOrdered(const vector<double> &angles){
     for (unsigned int i = 1; i < angles.size(); i++){
@@ -29,10 +38,36 @@ bool isOrdered(const vector<double> &angles){
 }
 
 //changes flight-path angles to ascending order 
-void reorder(vector<double> &angles, vector<double> &coefficients);
+void reorder(vector<double> &angles, vector<double> &coefficients){
+    //selection sort
+    double minAngle;
+    int minIndex;
+    double tempAngle;
+    double tempCoef;
+    for (unsigned int i = 0; i < angles.size(); i++){
+        minAngle = angles[i];
+        minIndex = i;
+        for (unsigned int ii = i; ii < angles.size() ; ii++){
+            
+            if (angles[ii] < minAngle){
+                minAngle = angles[ii];
+                minIndex = ii;
+            }
+        }
+
+        tempAngle = angles[i];
+        angles[i] = minAngle;
+        angles[minIndex] = tempAngle;
+
+        tempCoef = coefficients[i];
+        coefficients[i] = coefficients[minIndex];
+        coefficients[minIndex] = tempCoef;
+    }
+}
 
 int main(int argc, char *argv[]){
-    string fileName = argv[1];
+    //string fileName = argv[1];
+    string fileName = "tunnel2.dat";
     vector<double> angles;
     vector<double> coefficients; 
 
@@ -41,6 +76,10 @@ int main(int argc, char *argv[]){
         cout << angles[i] << " " << coefficients[i] << endl;
     }
     cout << "fisni" << endl;
+    reorder(angles,coefficients);
+    for (unsigned int i = 0; i < angles.size(); i++){
+        cout << angles[i] << " " << coefficients[i] << endl;
+    }
 
     return 0;
 
