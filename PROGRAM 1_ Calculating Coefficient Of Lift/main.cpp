305f7da5bebcee4b8a6294feeb733b5e27ba3246
TomeT187 <tomet187@gmail.com>
2024-01-23 23:32:48 -0800

    no clue whats on this

--- PROGRAM 1_ Calculating Coefficient Of Lift/main.cpp
+++ PROGRAM 1_ Calculating Coefficient Of Lift/main.cpp
@@ -8,6 +8,9 @@ using namespace std;
 void readData(const string &filename, vector<double> &angles, vector<double> &coefficients){
     ifstream fin(filename);
     double value;
+    if (!fin.is_open()){
+        cout << "Error opening " << filename << endl;
+    }
     while(fin >> value){
         angles.push_back(value);
         fin >> value;
@@ -16,24 +19,23 @@ void readData(const string &filename, vector<double> &angles, vector<double> &co
 
     fin.close();
 }
-double interpolation(double angle, const vector<double> &angles, const vector<double> &coefficients){
-    //return f(b) = f(a) + ((b - a)/(c - a))(f(c) - f(a)) or f(b) if exists
 
-    //if b exists return f(b)
-    for (unsigned int i = 1; i < angles.size(); i++){
+double interpolation(double angle, const vector<double> &angles, const vector<double> &coefficients){
+    for (unsigned int i = 0; i < angles.size(); i++){
         if (angle == angles[i]){
             return coefficients[i];
         }
     }
 
-    double a = angles[0]; //biggest min
-    int aIndex = 0;
+    //variable names follow given equation
+    double a = angles[0]; //value of biggest min
+    int aIndex = 0;       //index of biggest min
     // b = angle
-    double c = angles[angles.size() - 1]; //smallest max
-    int cIndex = angles.size() - 1;
+    double c = angles[angles.size() - 1]; //value of smallest max
+    int cIndex = angles.size() - 1;       //index of smallest max
 
-    for (unsigned int i = 1; i < angles.size(); i++){ 
-        if (angles[i] > a && angles[i] < angle){
+    for (unsigned int i = 0; i < angles.size(); i++){ 
+        if (angles[i] < angle && angles[i+1] > angle){
             a = angles[i];
             aIndex = i;
             c = angles[i + 1];
@@ -42,9 +44,9 @@ double interpolation(double angle, const vector<double> &angles, const vector<do
     }
     //return f(b) = f(a) +         ((   b  - a) / (c - a)) * (      f(c)           -        f(a)         )
     return (coefficients[aIndex] + ((angle - a) / (c - a)) * (coefficients[cIndex] - coefficients[aIndex]));
-
 }
 
+//returns true if each value of angle vector is larger than the last
 bool isOrdered(const vector<double> &angles){
     for (unsigned int i = 1; i < angles.size(); i++){
         // next is less than previous
@@ -55,13 +57,14 @@ bool isOrdered(const vector<double> &angles){
     return true;
 }
 
-//changes flight-path angles to ascending order 
+//changes flight-path angles to ascending order using selection sort
 void reorder(vector<double> &angles, vector<double> &coefficients){
     //selection sort
     double minAngle;
     int minIndex;
     double tempAngle;
     double tempCoef;
+
     for (unsigned int i = 0; i < angles.size(); i++){
         minAngle = angles[i];
         minIndex = i;
@@ -84,21 +87,37 @@ void reorder(vector<double> &angles, vector<double> &coefficients){
 }
 
 int main(int argc, char *argv[]){
-    //string fileName = argv[1];
-    string fileName = "tunnel2.dat";
+    string fileName = argv[1];
     vector<double> angles;
     vector<double> coefficients; 
 
+    ifstream fin(fileName);
+    if (!fin.is_open()){
+        cout << "Error opening " << fileName << endl;
+        fin.close();
+        return 0;
+    }
+    
     readData(fileName,angles,coefficients);
-    for (unsigned int i = 0; i < angles.size(); i++){
-        cout << angles[i] << " " << coefficients[i] << endl;
+
+    string input = "";
+    double inputAngle;
+    if (!isOrdered(angles)){
+        reorder(angles,coefficients);
+    }else{
+        double minAngle = angles[0];
+        double maxAngle = angles.back();
+        while (input != "No"){
+            cout << "Enter an angle:" << endl;
+            cin >> inputAngle;
+            if (inputAngle >= minAngle && inputAngle <= maxAngle){
+                cout << interpolation(inputAngle, angles, coefficients ) << endl;
+            }
+            cout << "Would you like to calculate again?" << endl;
+            cin >> input;
+        }
     }
-    cout << "fisni" << endl;
-    reorder(angles,coefficients);
-    for (unsigned int i = 0; i < angles.size(); i++){
-        cout << angles[i] << " " << coefficients[i] << endl;
-    }
-    cout << interpolation(8.0, ); // finnish testing
+
     return 0;
 
 }
