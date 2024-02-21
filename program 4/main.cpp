d01eef23076bdc634d624214a92979a230933374
TomeT187 <tomet187@gmail.com>
2024-02-20 16:06:27 -0800

    ok now im done

--- program 4/main.cpp
+++ program 4/main.cpp
@@ -7,41 +7,41 @@ int main(){
 
     //vector with size and capacity 10 filled with 10
     
-    int size;
-    int value;
-    cout << "Enter vector size" << endl;
-    cin >> size;
-    cout << "Enter the value for all indices" << endl;
-    cin >> value;
-    IntVector test(size,value);
+    // int size;
+    // int value;
+    // cout << "Enter vector size" << endl;
+    // cin >> size;
+    // cout << "Enter the value for all indices" << endl;
+    // cin >> value;
+    // IntVector test(size,value);
 
-    cout << "Should output " << size << " for both" << endl;
-    cout << "Test size: " << test.size() << endl;
-    cout << "Test capacity: " << test.capacity() << endl << endl;
+    // cout << "Should output " << size << " for both" << endl;
+    // cout << "Test size: " << test.size() << endl;
+    // cout << "Test capacity: " << test.capacity() << endl << endl;
 
-    cout << "Should output " << false << endl;
-    cout << "Test is not empty " << test.empty() << endl << endl;
+    // cout << "Should output " << false << endl;
+    // cout << "Test is not empty " << test.empty() << endl << endl;
     
-    int index;
-    cout << "Enter an index to check" << endl;
-    cin >> index;
-    cout << "If index is out of bounds, program will abort" << endl;
-    cout << "Should output " <<  value <<" at index " << index << endl;
-    cout << "Value at index " << index << ": " << test.at(index) << endl << endl;
+    // int index;
+    // cout << "Enter an index to check" << endl;
+    // cin >> index;
+    // cout << "If index is out of bounds, program will abort" << endl;
+    // cout << "Should output " <<  value <<" at index " << index << endl;
+    // cout << "Value at index " << index << ": " << test.at(index) << endl << endl;
 
-    cout << "Should output " << value << " for both front and back" << endl;
-    cout << "Value at Front: " << test.front() << endl;
-    cout << "Value at Back: " << test.back() << endl << endl;
+    // cout << "Should output " << value << " for both front and back" << endl;
+    // cout << "Value at Front: " << test.front() << endl;
+    // cout << "Value at Back: " << test.back() << endl << endl;
 
-    //test with an empty vector
-    IntVector emptyTest(0,5);
-    cout << "Should output " << true << endl;
-    cout << "Test is  empty " << emptyTest.empty()  << endl << endl;
+    // //test with an empty vector
+    // IntVector emptyTest(0,5);
+    // cout << "Should output " << true << endl;
+    // cout << "Test is  empty " << emptyTest.empty()  << endl << endl;
 
 
-    cout << "Will abort the program after this test is ran and display error message" << endl;
-    int outOfBonds = size + 1;
-    cout << "Value out of Bounds: " << emptyTest.at(outOfBonds);
+    // cout << "Will abort the program after this test is ran and display error message" << endl;
+    // int outOfBonds = size + 1;
+    // cout << "Value out of Bounds: " << emptyTest.at(outOfBonds);
 
 
     IntVector test(5,5);
@@ -131,19 +131,19 @@ int main(){
    cout << "with lower reserve" << endl;
    test.reserve( 5);
    cout << test.capacity() << endl;
-   IntVector temp;
-    cout << "start";
-    temp.push_back(5);
-    cout << "pushed";
-    cout << temp.at(0);
-    IntVector* ptr = new IntVector;
-    delete ptr;
-    cout << "deleted2";
-    IntVector test;
-    test.insert(1,100);
-    for (unsigned i = 0; i < test.size(); i++){
-        cout << test.at(i);
-    }
+//    IntVector temp;
+//     cout << "start";
+//     temp.push_back(5);
+//     cout << "pushed";
+//     cout << temp.at(0);
+//     IntVector* ptr = new IntVector;
+    //delete ptr;
+    // cout << "deleted2";
+    // IntVector test;
+    // test.insert(1,100);
+    // for (unsigned i = 0; i < test.size(); i++){
+    //     cout << test.at(i);
+    // }
 
 
     
